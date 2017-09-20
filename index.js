8ac06da0d77353f635b6237e164687f34c67db90
Josh Duff <me@JoshDuff.com>
2017-09-20 14:38:54 -0500

    Some prettier functions, maybe!

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var fetch = require('node-fetch')
 var requestToObject = require('./lib/request-to-object')
 var fetchMiddleware = require('./lib/fetch-middleware')
 
-var Fetchware = function (middleware) {
-  var fetchware = function (input, init) {
+var Fetchware = (middleware) => {
+  var fetchware = (input, init) => {
     // normalize and clone by passing through Request constructor, then pull properties off of request instance because they are read-only
     var request = requestToObject(new fetch.Request(input, init))
     return compose(middleware.concat(fetchMiddleware))(request)
   }
-  fetchware.use = function (fnOrArray) {
+  fetchware.use = (fnOrArray) {
     return Fetchware(middleware.concat(fnOrArray))
   }
   return fetchware
